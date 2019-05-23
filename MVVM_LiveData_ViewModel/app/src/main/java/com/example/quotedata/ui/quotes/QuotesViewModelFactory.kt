fa6674cf4ef62ad26c527595553444ef58ede523
Nguyen Khanh long <nguyen.khanh.long@framgia.com>
2019-05-23 18:02:53 +0700

    switch branch develop

--- MVVM_LiveData_ViewModel/app/src/main/java/com/example/quotedata/ui/quotes/QuotesViewModelFactory.kt
+++ MVVM_LiveData_ViewModel/app/src/main/java/com/example/quotedata/ui/quotes/QuotesViewModelFactory.kt
@@ -4,6 +4,8 @@ import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import com.example.quotedata.data.QuoteRepository
 
+//The same repository that's needed for QuotesViewModel
+//is also passed to the factory
 class QuotesViewModelFactory(private val quoteRepository: QuoteRepository) :ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
